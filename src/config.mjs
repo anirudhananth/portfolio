441023ea37cbc4d027b6ce048503e2efdcb0e174
Ani <anirudhananth99@gmail.com>
2022-10-11 17:51:51 +0530

    First portfolio changes

--- src/config.mjs
+++ src/config.mjs
@@ -1,10 +1,10 @@
 export const SITE = {
-	name: 'AstroWind',
+	name: 'Anirudh Ananth',
 
 	origin: 'https://astrowind.vercel.app',
 	basePathname: '/',
 
-	title: 'AstroWind — Your website with Astro + Tailwind CSS',
+	title: 'Anirudh Ananthasubramaniam',
 	description: '🚀 AstroWind is a free and ready to start template to make your website using Astro and Tailwind CSS.',
 
 	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
